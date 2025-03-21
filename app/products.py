fb10cdcb7ad8759eb2ec9a800faca205deafd2a5
Tomasz Wiszowaty <twiszowaty@amplifier.pl>
2025-03-21 14:17:03 +0100

    prices import, data types

--- app/products.py
+++ app/products.py
@@ -1,4 +1,4 @@
-import decimal
+from decimal import Decimal
 
 from amper_api.product import Product, UnitOfMeasure
 from app.trawers_commons import get_records_trawers
@@ -6,6 +6,16 @@ from app.trawers_commons import get_records_trawers
 def import_products(backend):
     fields = ['indeks', 'inazwa', 'nrptu', 'jm', 'jmz', 'opilosc', 'waga', 'ean13', 'opakowan']
     records = get_records_trawers(system='MG', table_id='229', fields=fields)
+    fields = ['indeks', 'typ', 'spcena1', 'spcena3']
+    prices = get_records_trawers(system='MI', table_id='211', fields=fields)
+    prices_table = []
+    for price in prices:
+        if price['INDEKS'] and price['TYP'] == '60':
+            prices_table.append(
+                {"INDEX": price['INDEKS'],
+                 "PRICE": price['SPCENA1'],
+                 "MINIMAL_PRICE": price['SPCENA3']}
+            )
     vat = {'00': 0,
            '01': 23,
            '02': 8,
@@ -15,12 +25,18 @@ def import_products(backend):
            '06': 23,
            '98': 0,
            '99': 0}
+
     products = []
     unit_of_measures = []
     for record in records:
-        #todo ustalić skąd ma być pobierana cena katalogowa (cena 100) i cena minimalna (jeżeli taka jest)
-        cena = 0
-        min_price = 0
+        price = "0"
+        min_price = "0"
+        for price in prices_table:
+            if price['INDEX'] == record['INDEKS']:
+                price = price['PRICE']
+                min_price = price['MINIMAL_PRICE']
+                break
+
         product = Product()
         product.updatable_fields = 'short_code,ean,sku,default_price,minimal_price,default_unit_of_measure,vat,piggy_bank_budget,weight'
         product.attributes = []
@@ -34,29 +50,29 @@ def import_products(backend):
         product.available_on = '2000-01-01'
         product.is_published = True
         product.is_featured = False
-        product.weight = record['WAGA']
+        product.weight = Decimal(record['WAGA'])
         product.default_unit_of_measure = record['JM']
         product.external_id = record['INDEKS']
         product.cumulative_unit_of_measure = 'Op.zb.'
-        product.cumulative_converter = record['OPILOSC']
+        product.cumulative_converter = Decimal(record['OPILOSC'])
         product.can_be_split = False
-        product.cumulative_unit_ratio_splitter = 1
+        product.cumulative_unit_ratio_splitter = Decimal("1")
         product.unit_roundup = False
         product.ean = record['EAN13']
-        product.default_price = cena
-        product.minimal_price = min_price
+        product.default_price = Decimal(price)
+        product.minimal_price = Decimal(min_price)
 
         products.append(product)
 
-        if record['OPILOSC'] and decimal.Decimal(record['OPILOSC']) > 0 and record['OPAKOWAN'] is not None:
+        if record['OPILOSC'] and Decimal(record['OPILOSC']) > 0 and record['OPAKOWAN'] is not None:
             unit_of_measure = UnitOfMeasure()
             unit_of_measure.external_id = f'opzb_{record["INDEKS"]}'
             unit_of_measure.product_external_id = record['INDEKS']
             unit_of_measure.can_be_split = False
-            unit_of_measure.converter = record['OPILOSC']
-            unit_of_measure.cumulative_unit_ratio_splitter = 1
+            unit_of_measure.converter = Decimal(record['OPILOSC'])
+            unit_of_measure.cumulative_unit_ratio_splitter = Decimal("1")
             unit_of_measure.name = record['OPAKOWAN']
-            unit_of_measure.weight = 0
+            unit_of_measure.weight = Decimal("0")
             unit_of_measure.unit_roundup = False
             unit_of_measures.append(unit_of_measure)
 
